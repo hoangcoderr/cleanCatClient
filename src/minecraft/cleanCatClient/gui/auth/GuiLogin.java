f13e20ede9eb9ea8ab430930a7ff6538152a81d0
Hoang Kevin <dhoang1234sp@gmail.com>
2025-01-24 08:55:43 +0700

    Fix bugs from alt manager

--- src/minecraft/cleanCatClient/gui/auth/GuiLogin.java
+++ src/minecraft/cleanCatClient/gui/auth/GuiLogin.java
@@ -1,4 +1,6 @@
 package cleanCatClient.gui.auth;
+
+import cleanCatClient.gui.mainmenu.MainMenu;
 import cleanCatClient.gui.mainmenu.button.ClientButton;
 import cleanCatClient.gui.notification.Notification;
 import cleanCatClient.gui.notification.NotificationManager;
@@ -22,11 +24,14 @@ public class GuiLogin extends GuiScreen {
                 SessionChanger.getInstance().setUserOffline(this.username.getText());
                 NotificationManager.show(new Notification("Logged in as " + this.username.getText(), 3));
             }
+        } else if (button.id == 1) {
+            this.mc.displayGuiScreen(new MainMenu());
         }
     }
 
     @Override
     public void drawScreen(final int x2, final int y2, final float z2) {
+        this.drawDefaultBackground();
         super.drawScreen(x2, y2, z2);
         final ScaledResolution sr = new ScaledResolution(this.mc);
         this.username.drawTextBox();
@@ -40,6 +45,7 @@ public class GuiLogin extends GuiScreen {
         final ScaledResolution sr = new ScaledResolution(this.mc);
         this.buttonList.clear();
         this.buttonList.add(new ClientButton(0, this.width / 2 - 50 - 10, this.height / 2 - 20, 120, 20, I18n.format("Login (Cracked)", new Object[0])));
+        this.buttonList.add(new ClientButton(1, this.width / 2 - 50 - 10, this.height / 2 + 10, 120, 20, "Back to Main Screen"));
         (this.username = new GuiTextField(100, this.fontRendererObj, this.width / 2 - 50 - 10, sr.getScaledHeight() / 2 - 50, 120, 20)).setFocused(true);
         Keyboard.enableRepeatEvents(true);
     }
